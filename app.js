a0db892ba419cbc320f213e2728c66d736d4ffc5
Anshitha <anshithavelagapudi@gmail.com>
2020-04-17 16:00:20 -0500

    edited in app.js

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-let catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
-
-let compression = require('compression');
-let helmet = require('helmet');
-
-let app = express();
-
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+const dotenv = require('dotenv')
+const app = express();
+dotenv.config({ path: '.env' })
 
 // Set up mongoose connection
-let mongoose = require('mongoose');
-let dev_db_url = 'mongodb+srv://<anshitha velagapudi>:<ammulusmiley>@cluster0-wifx7.azure.mongodb.net/local_library?retryWrites=true&w=majority'
-
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb+srv://<anshitha velagapudi>:<ammulusmiley>@cluster0-wifx7.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true ,useUnifiedTopology:true});
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+dev_db_url = process.env.ATLAS_URI
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,8 +27,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(helmet());
-app.use(compression()); // Compress all routes
 
 app.use(express.static(path.join(__dirname, 'public')));
 
